0b10ce2665d192d1f8374bb77d2c38bcbd32af05
Anwesh43 <anweshthecool0@gmail.com>
2019-11-06 11:02:34 +0530

    adding ArcBouncyView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedquarterarcbouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedquarterarcbouncyview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedquarterarcbouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.quarterarcbouncyview.ArcBouncyView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ArcBouncyView.create(this)
     }
 }
