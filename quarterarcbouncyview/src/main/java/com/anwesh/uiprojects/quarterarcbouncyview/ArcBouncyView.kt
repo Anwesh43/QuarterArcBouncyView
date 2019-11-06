62371ed4c702616b2598b489aee692e41059dbd0
Anwesh43 <anweshthecool0@gmail.com>
2019-11-06 10:42:16 +0530

    adding ArcBouncyView class

--- quarterarcbouncyview/src/main/java/com/anwesh/uiprojects/quarterarcbouncyview/ArcBouncyView.kt
+++ quarterarcbouncyview/src/main/java/com/anwesh/uiprojects/quarterarcbouncyview/ArcBouncyView.kt
@@ -59,3 +59,21 @@ fun Canvas.drawABNode(i : Int, scale : Float, paint : Paint) {
     drawArcLine(i, size, scale, paint)
     restore()
 }
+
+class ArcBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
